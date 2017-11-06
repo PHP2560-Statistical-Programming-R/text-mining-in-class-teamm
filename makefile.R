52e3d2b9a56be5a9b114f7442b2feb038073a13d
Olivia Iles <olivia_iles@brown.edu>
2017-11-06 14:25:11 -0500

    updated makefile so linde_graphs is created upon running it and it knits markdown!

--- makefile.R
+++ makefile.R
@@ -1,7 +1,7 @@
 unlink("data", recursive = TRUE) # where data is stored after clean
 unlink("olivia_results",recursive=TRUE)
 unlink("graph", recursive = TRUE) # where graphs are stores
-unlink("linde_graph", recursive = TRUE) # where graphs are stores
+unlink("linde_graphs", recursive = TRUE) # where graphs are stores
 
 dir.create(file.path("data"), showWarnings = FALSE)
 dir.create(file.path("olivia_results"), showWarnings = FALSE)
@@ -15,3 +15,4 @@ source("olivia_analysis.R")
 source("linde-analysis.R")
 source("olivia_vis.R")
 rmarkdown::render("olivia_paper.Rmd", output_format = "html_document")
+rmarkdown::render("linde_paper.Rmd", output_format = "html_document")
