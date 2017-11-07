55ffe7c28c4276ec44d778032e9c8500ad18663e
Olivia Iles <olivia_iles@brown.edu>
2017-11-07 17:02:53 -0500

    fixed graphs

--- anna-graphData.R
+++ anna-graphData.R
@@ -10,7 +10,7 @@ ggplot(nyt.sent,aes(title.score)) +
   labs(x = "Mean Score") +
   geom_histogram(color="black",fill="lightblue1",aes(y=..density..), binwidth=1) +
   geom_density() + 
-  scale_x_continuous(breaks = pretty(scores$title.score, n = 10)) +
+  scale_x_continuous(breaks = pretty(nyt.sent$title.score, n = 10)) +
   ggtitle("NY Times Title Mean Sentiment Scores Distribution") +
   theme(plot.title = element_text(color="black", face="bold", size=18,hjust = 0.5)) +
   #theme_bw() + 
@@ -32,7 +32,7 @@ ggplot(nyt.intensity,aes(mean.intensity)) +
   geom_histogram(color="black",fill="indianred2",aes(y=..density..), binwidth=0.5) +
   geom_density() + 
   # label the x-axis tick marks in a way that's intuitive
-  scale_x_continuous(breaks = pretty(scores$title.intensity.score, n = 10)) +
+  scale_x_continuous(breaks = pretty(nyt.intensity$mean.intensity, n = 10)) +
   # make the title nice
   ggtitle("NY Times Title Mean Intensity Scores Distribution") +
   theme(plot.title = element_text(color="black", face="bold", size=18,hjust = 0.5)) +
@@ -51,11 +51,11 @@ ggsave("anna_graphs/title.intensity_graph.png")
 
 ####### Plot Mean Sentiment Scores
 # This plots the sentiment scores
-ggplot(data=year.sent, aes(x=year.num,y=title.score)) +
+ggplot(data=year.sent, aes(x=year.num,y=year.sent)) +
   geom_line(color="steelblue4", linetype="dashed") +
   labs(x = "Year", y = "Mean Score") +
-  scale_x_continuous(breaks = pretty(scores2$year.num, n = 10),labels=c(1996:2006)) +
-  scale_y_continuous(breaks = pretty(scores2$year.score, n = 8)) +
+  scale_x_continuous(breaks = pretty(year.sent$year.num, n = 10),labels=c(1996:2006)) +
+  scale_y_continuous(breaks = pretty(year.sent$year.sent, n = 8)) +
   geom_point(size=3,color="steelblue4") +
   ggtitle("NY Times Mean Title Sentiment Scores Over Time") +
   theme(plot.title = element_text(color="black", face="bold", size=18,hjust = 0.5)) +
@@ -75,8 +75,8 @@ ggsave("anna_graphs/time.sent_graph.png")
 intensity.time_graph = ggplot(data=year.intensity, aes(x=year.num,y=intensity)) +
   geom_line(color="firebrick3", linetype="dashed") +
   labs(x = "Year", y = "Mean Intensity") +
-  scale_x_continuous(breaks = pretty(scores3$year.num, n = 10),labels=c(1996:2006)) +
-  scale_y_continuous(breaks = pretty(scores3$year.intensity, n = 8)) +
+  scale_x_continuous(breaks = pretty(year.intensity$year.num, n = 10),labels=c(1996:2006)) +
+  scale_y_continuous(breaks = pretty(year.intensity$intensity, n = 8)) +
   geom_point(size=3,color="firebrick3") +
   ggtitle("NY Times Mean Title Sentiment Intensity Over Time") +
   theme(plot.title = element_text(color="black", face="bold", size=18,hjust = 0.5)) +
