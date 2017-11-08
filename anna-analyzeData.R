f5d39b85f79fe2fecec7fd3736435e6bb7620beb
anna-xu <anna_xu@brown.edu>
2017-11-07 23:46:18 -0500

    graphs and data analysis

--- anna-analyzeData.R
+++ anna-analyzeData.R
@@ -1,4 +1,6 @@
 load("data/nyt_data.rda")
+source("check_packages.R")
+check_packages(c("tidytext","stringr","dplyr","rebus","ggplot2")) #loads packages
 
 ############## PART 1 ##############
 
@@ -21,12 +23,7 @@ nyt.sent = NYTimes %>%
   # get a mean of the score for each word in the title to get an average sentiment score for each title 
   summarize(title.score = mean(score))
 
-# SAVE ALL THIS
-save(nyt.sent, file = "data/nyt_sentimentScores")
-
 ####### INTENSITY #######
-
-load("data/nyt_sentimentScores")
 
 # Get absolute value
 nyt.sent = nyt.sent %>%
@@ -37,9 +34,6 @@ nyt.sent = nyt.sent %>%
 nyt.intensity = nyt.sent %>%
   group_by(Title) %>%
   summarize(mean.intensity = mean(intensity))
-
-# SAVE ALL THIS
-save(nyt.intensity, file = "data/nyt_intensityScores")
 
 ############## PART 2 ##############
 
@@ -72,32 +66,118 @@ year.sent = NYTimes %>%
   # get the sentiment scores for each word in the title 
   inner_join(sentiment.scores) %>%
   # get a mean of the score for each word in the title to get an average sentiment score for each title 
-  summarize(title.score = mean(score))
+  summarize(title = mean(score))
 
 # Now, get mean sentiment scores for years
-year.sent = year.sent %>%
+mean.year.sent = year.sent %>%
   group_by(year) %>%
-  summarize(year.sent = mean(title.score))
+  summarize(year.sent = mean(title))
 
 ####### INTENSITY #######
 
 # create dataframe containing the intensity
-year.intensity = year.sent %>%
+year.intensity = mean.year.sent %>%
   mutate(intensity = abs(year.sent))
 
 ####### CLEAN YEARS #######
 
 # change years to numeric now, first in the year.sent dataframe
-year.sent$year = as.numeric(year.sent$year)
+mean.year.sent$year = as.numeric(mean.year.sent$year)
 
 # and make it go from year 1 to year 10 (with 1996 as year 1)
-year.sent = year.sent %>%
-  mutate(year.num = ifelse(year.sent$year<=6, 10-year.sent$year, abs(96-year.sent$year)))
+mean.year.sent = mean.year.sent %>%
+  mutate(year.num = ifelse(year<=6, 10-year, abs(96-year)))
 
 # now also save these year numbers into year.intensity dataframe
-year.intensity$year.num = year.sent$year.num
+year.intensity$year.num = mean.year.sent$year.num
 
-####### SAVE ALL THE DATA #######
 
-save(year.sent, file = "data/nyt_yearSentiment")
-save(year.intensity, file = "data/nyt_yearIntensity")
+############## GRAPH EVERYTHING ##############
+
+# plot mean sentiment scores for each title 
+
+ggplot(nyt.sent,aes(title.score)) + 
+  labs(x = "Mean Score") +
+  geom_histogram(color="black",fill="lightblue1",aes(y=..density..), binwidth=1) +
+  geom_density() + 
+  scale_x_continuous(breaks = pretty(nyt.sent$title.score, n = 10)) +
+  ggtitle("NY Times Title Mean Sentiment Scores Distribution") +
+  theme(plot.title = element_text(color="black", face="bold", size=18,hjust = 0.5)) +
+  #theme_bw() + 
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(panel.background = element_rect(fill='white')) +
+  theme(axis.text.x = element_text(colour="grey20",size=12,angle=45,hjust=.5,vjust=.5,face="plain"),
+        axis.text.y = element_text(colour="grey20",size=12,angle=0,hjust=1,vjust=0,face="plain"),  
+        axis.title.x = element_text(colour="grey20",size=15,angle=0,hjust=.5,vjust=0,face="plain"),
+        axis.title.y = element_text(colour="grey20",size=15,angle=90,hjust=.5,vjust=.5,face="plain"))
+
+#SAVE
+ggsave("anna_graphs/title.sent_graph.png")
+
+ggplot(nyt.intensity,aes(mean.intensity)) + 
+  # label the x-axis 
+  labs(x = "Intensity Score") +
+  # create histogram with a density curve
+  geom_histogram(color="black",fill="indianred2",aes(y=..density..), binwidth=0.5) +
+  geom_density() + 
+  # label the x-axis tick marks in a way that's intuitive
+  scale_x_continuous(breaks = pretty(nyt.intensity$mean.intensity, n = 10)) +
+  # make the title nice
+  ggtitle("NY Times Title Mean Intensity Scores Distribution") +
+  theme(plot.title = element_text(color="black", face="bold", size=18,hjust = 0.5)) +
+  # get the background to be white and get rid of the grid marks
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(panel.background = element_rect(fill='white')) +
+  # make the axis tick marks and text look nicer 
+  theme(axis.text.x = element_text(colour="grey20",size=12,angle=45,hjust=.5,vjust=.5,face="plain"),
+        axis.text.y = element_text(colour="grey20",size=12,angle=0,hjust=1,vjust=0,face="plain"),  
+        axis.title.x = element_text(colour="grey20",size=15,angle=0,hjust=.5,vjust=0,face="plain"),
+        axis.title.y = element_text(colour="grey20",size=15,angle=90,hjust=.5,vjust=.5,face="plain"))
+
+#SAVE
+ggsave("anna_graphs/title.intensity_graph.png")
+
+####### Plot Mean Sentiment Scores
+# This plots the sentiment scores
+ggplot(data=mean.year.sent, aes(x=year.num,y=year.sent)) +
+  geom_line(color="steelblue4", linetype="dashed") +
+  labs(x = "Year", y = "Mean Score") +
+  scale_x_continuous(breaks = pretty(year.sent$year.num, n = 10),labels=c(1996:2006)) +
+  scale_y_continuous(breaks = pretty(year.sent$year.score, n = 8)) +
+  geom_point(size=3,color="steelblue4") +
+  ggtitle("NY Times Mean Title Sentiment Scores Over Time") +
+  theme(plot.title = element_text(color="black", face="bold", size=18,hjust = 0.5)) +
+  #theme_bw() + 
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(panel.background = element_rect(fill='white')) +
+  theme(axis.text.x = element_text(colour="grey20",size=12,angle=0,hjust=.5,vjust=.5,face="plain"),
+        axis.text.y = element_text(colour="grey20",size=12,angle=0,hjust=1,vjust=0,face="plain"),  
+        axis.title.x = element_text(colour="grey20",size=15,angle=0,hjust=.5,vjust=0,face="plain"),
+        axis.title.y = element_text(colour="grey20",size=15,angle=90,hjust=.5,vjust=.5,face="plain"))
+
+#SAVE
+ggsave("anna_graphs/time.sent_graph.png")
+
+# This plots the intensity scores
+intensity.time_graph = ggplot(data=year.intensity, aes(x=year.num,y=intensity)) +
+  geom_line(color="firebrick3", linetype="dashed") +
+  labs(x = "Year", y = "Mean Intensity") +
+  scale_x_continuous(breaks = pretty(year.intensity$year.num, n = 10),labels=c(1996:2006)) +
+  scale_y_continuous(breaks = pretty(year.intensity$year.intensity, n = 8)) +
+  geom_point(size=3,color="firebrick3") +
+  ggtitle("NY Times Mean Title Sentiment Intensity Over Time") +
+  theme(plot.title = element_text(color="black", face="bold", size=18,hjust = 0.5)) +
+  #theme_bw() + 
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(panel.background = element_rect(fill='white')) +
+  theme(axis.text.x = element_text(colour="grey20",size=12,angle=0,hjust=.5,vjust=.5,face="plain"),
+        axis.text.y = element_text(colour="grey20",size=12,angle=0,hjust=1,vjust=0,face="plain"),  
+        axis.title.x = element_text(colour="grey20",size=15,angle=0,hjust=.5,vjust=0,face="plain"),
+        axis.title.y = element_text(colour="grey20",size=15,angle=90,hjust=.5,vjust=.5,face="plain"))
+
+#SAVE
+ggsave("anna_graphs/time.intensity_graph.png")
